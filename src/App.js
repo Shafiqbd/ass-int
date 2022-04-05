0cf9c7bfa4211b8f25f6d7e8bbfef6f283171ab9
Office <pridesys.8415@gmail.com>
2022-04-05 08:18:03 +0600

    select design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Grid } from "@mui/material";
 import Layout from "./components/layouts/Layout";
-
 function App() {
   return (
     <div className="App">
-      <Layout>Hello</Layout>
+      <Layout>
+        <Grid container>
+          <Grid item md={2}>
+            menu
+          </Grid>
+          <Grid item md={10}>
+            Body
+          </Grid>
+        </Grid>
+      </Layout>
     </div>
   );
 }
---
+++ src/components/OrderBy.js
@@ -0,0 +1,5 @@
+const OrderBy = () => {
+  return <div>List</div>;
+};
+
+export default OrderBy;
